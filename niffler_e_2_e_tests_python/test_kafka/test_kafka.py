114a39241beffedd1cfd5f60a306c31c50afe868
sdafDHSUF76 <jkFSDzeuryn@mail.ru>
2025-01-30 21:25:14 +0400

    1

--- niffler_e_2_e_tests_python/test_kafka/test_kafka.py
+++ niffler_e_2_e_tests_python/test_kafka/test_kafka.py
@@ -1,17 +1,16 @@
 import json
-from http import HTTPStatus
-from typing import TYPE_CHECKING
+import time
+from typing import TYPE_CHECKING, Callable
 
+import pytest
 from allure import epic, feature, title
 from faker import Faker
 from test_kafka.enums.Topics import Topics
 from test_kafka.models.authorization_data import AuthorizationDataUser
-from tests_api.clients_api.user import User
 
 if TYPE_CHECKING:
     from client.kafka_client import KafkaClient
     from confluent_kafka import TopicPartition
-    from requests import Response
     from utils.database import DB
 
 
@@ -19,22 +18,43 @@ if TYPE_CHECKING:
 @feature("[KAFKA][niffler-auth]: Паблишинг сообщений в кафку в топик \'users\'")
 class TestAuthRegistrationKafkaTest:
 
+    @pytest.fixture
+    def check_appearance_of_data_in_userdate(self, db_niffler_userdata: 'DB') -> Callable:
+        def _method(expected_user_name: str) -> None:
+            attempt = 3
+            sleep = 2
+            for _ in range(attempt):
+                try:
+                    assert db_niffler_userdata.get_value(
+                        'select 1 from "user" where username = \'%s\'' % expected_user_name
+                    ), (
+                        'В базе: \'niffler-userdata\' данных не нашлось совпадений с'
+                        f' {expected_user_name} в столбце username'
+                    )
+                except AssertionError:
+                    time.sleep(sleep)
+                    continue
+        return _method
+
     @title("KAFKA: Сообщение с пользователем публикуется в Kafka после успешной регистрации")
     def test_message_should_be_produced_to_kafka_after_successful_registration(
-        self, client_kafka: 'KafkaClient', db_niffler_userdata: 'DB',
+        self,
+        client_kafka: 'KafkaClient',
+        db_niffler_userdata: 'DB',
+        check_appearance_of_data_in_userdate: Callable,
     ):
         expected_user_name: str = Faker().user_name()
-        password: str = Faker().password(special_chars=False)
         topic_partitions: list['TopicPartition'] = client_kafka.subscribe_listen_new_offsets(
             Topics.USERS.value,
         )
-        client_kafka.producer.produce(Topics.USERS.value, json.dumps({'username': expected_user_name}))
-        # client_kafka.producer.send_offsets_to_transaction()
-
-        # response: 'Response' = User().create_user(expected_user_name, password)
-
-        # assert response.status_code == HTTPStatus.CREATED
+        users_data = {'username': expected_user_name}
+        client_kafka.producer.produce(
+            Topics.USERS.value,
+            json.dumps(users_data).encode('utf-8'),
+            headers={'__TypeId__': 'guru.qa.niffler.model.UserJson'},
+        )
         event: bytes | None = client_kafka.log_msg_and_json(topic_partitions)
+
         assert event != b'' and event is not None, (
             f'сообщения в kafka нету! Проверьте топик: {Topics.USERS.value}.'
         )
@@ -45,9 +65,4 @@ class TestAuthRegistrationKafkaTest:
             f'получили сообщение: {current_authorization_user}  из топика: {Topics.USERS.value} и '
             f'оно не совпало с ожидаемым: {expected_user_name}'
         )
-        assert db_niffler_userdata.get_value(
-            'select 1 from "user" where username = \'%s\'' % expected_user_name
-        ), (
-            f'В базе: \'niffler-userdata\' данных не нашлось совпадений с {expected_user_name} '
-            'в столбце username'
-        )
+        check_appearance_of_data_in_userdate(expected_user_name)
