7a8f7cd194cf32ddcc95a2932268812e84ad3598
sdafDHSUF76 <jkFSDzeuryn@mail.ru>
2025-02-25 23:27:52 +0400

    Удалил модель, так как достаточно базу данных проверить

--- niffler_e_2_e_tests_python/test_kafka/models/authorization_data.py
+++
@@ -1,8 +0,0 @@
-import pydantic
-from pydantic import BaseModel, ConfigDict
-
-
-class AuthorizationDataUser(BaseModel):
-    model_config = ConfigDict(extra='forbid')
-
-    user_name: str = pydantic.Field(alias='username')
